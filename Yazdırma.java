1de2b9f0ed48b2d96c17addf6d607999d0a1c8c8
AhmetEmirCetin <146943192+AhmetEmirCetin@users.noreply.github.com>
2023-12-28 17:25:58 +0300

    Yazdırma Denemeleri

---
+++ Yazdırma.java
@@ -0,0 +1,12 @@
+
+public class Yazdırma {
+
+	public static void main(String[] args) {
+		System.out.println("Merhaba Dünya");
+		System.out.print("Merhaba Dünya");
+     /* Burada "print" ve "println" arasındakı farkı görüyoruz.
+      * "println" yazdıktan sonra otomatik olarak alt satıra geçer.
+      * "print" yazıldıktan sonta ise olduğu satırdan yazmaya devam eder.*/
+	}
+
+}
